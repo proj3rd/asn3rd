ddf5f28966673a37206a80c160219af0500f4923
Seokseong Jeon <sjeon87+github@gmail.com>
2022-03-29 23:15:16 +0900

    fix parse() not returning ModuleDefinitionsContext

--- dist/parser.d.ts
+++ dist/parser.d.ts
@@ -1,4 +1,4 @@
-import grammar3rdParser from "./parser/grammar3rdParser.js";
+import { ModuleDefinitionsContext } from "./parser/grammar3rdParser.js";
 import { Result } from "./types";
 /**
  * Data structure to store syntax error reported from parsre
@@ -19,6 +19,6 @@ declare class ParserError extends Error {
  * Parse ASN.1 definitino
  * @param text ASN.1 definition
  */
-export declare function parse(text: string): Result<grammar3rdParser, ParserError>;
+export declare function parse(text: string): Result<ModuleDefinitionsContext, ParserError>;
 export {};
 //# sourceMappingURL=parser.d.ts.map
