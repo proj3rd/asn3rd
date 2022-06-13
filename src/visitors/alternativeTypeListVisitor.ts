9f1ab0e6fd2a59bbb862114e675272fe7654963f
Seokseong Jeon <sjeon87+github@gmail.com>
2022-06-14 01:04:26 +0900

    add AlternativeTypeListVisitor

--- src/visitors/alternativeTypeListVisitor.ts
+++ src/visitors/alternativeTypeListVisitor.ts
@@ -1,11 +1,20 @@
-import { AlternativeTypeListContext } from "../parser/asn1Parser";
+import {
+  AlternativeTypeListContext,
+  NamedTypeContext,
+} from "../parser/asn1Parser";
+import { NamedTypeVisitor } from "./namedTypeVisitor";
 
 /**
  * Grammar
  * ```
+ * namedType (COMMA namedType)*
  * ```
  */
 export class AlternativeTypeListVisitor {
   public visitChildren(ctx: AlternativeTypeListContext) {
+    const nameTypeCtxList = ctx.namedType();
+    return nameTypeCtxList.map((nameTypeCtx: NamedTypeContext) =>
+      nameTypeCtx.accept(new NamedTypeVisitor())
+    );
   }
 }
