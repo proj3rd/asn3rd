3eb5ca7ae2bbe144c1a0307890954e32d8d665b1
Seokseong Jeon <sjeon87+github@gmail.com>
2022-06-14 01:10:44 +0900

    add AssignmentVisitor

--- src/visitors/assignmentVisitor.ts
+++ src/visitors/assignmentVisitor.ts
@@ -1,11 +1,42 @@
 import { AssignmentContext } from "../parser/asn1Parser";
+import { ObjectAssignmentVisitor } from "./objectAssignmentVisitor";
+import { ObjectClassAssignmentVisitor } from "./objectClassAssignmentVisitor";
+import { ObjectSetAssignmentVisitor } from "./objectSetAssignmentVisitor";
+import { ParameterizedAssignmentVisitor } from "./parameterizedAssignmentVisitor";
+import { TypeAssignmentVisitor } from "./typeAssignmentVisitor";
+import { ValueAssignmentVisitor } from "./valueAssignmentVisitor";
 
 /**
  * Grammar
  * ```
+ * typeAssignment | valueAssignment | objectClassAssignment | objectAssignment | objectSetAssignment | parameterizedAssignment
  * ```
  */
 export class AssignmentVisitor {
   public visitChildren(ctx: AssignmentContext) {
+    const typeAssignmentCtx = ctx.typeAssignment();
+    if (typeAssignmentCtx) {
+      return typeAssignmentCtx.accept(new TypeAssignmentVisitor());
+    }
+    const valueAssignmentCtx = ctx.valueAssignment();
+    if (valueAssignmentCtx) {
+      return valueAssignmentCtx.accept(new ValueAssignmentVisitor());
+    }
+    const objectClassAssignmentCtx = ctx.objectClassAssignment();
+    if (objectClassAssignmentCtx) {
+      return objectClassAssignmentCtx.accept(new ObjectClassAssignmentVisitor());
+    }
+    const objectAssignmentCtx = ctx.objectAssignment();
+    if (objectAssignmentCtx) {
+      return objectAssignmentCtx.accept(new ObjectAssignmentVisitor());
+    }
+    const objectSetAssignmentCtx = ctx.objectSetAssignment();
+    if (objectSetAssignmentCtx) {
+      return objectSetAssignmentCtx.accept(new ObjectSetAssignmentVisitor());
+    }
+    const parameterizedAssignmentCtx = ctx.parameterizedAssignment();
+    if (parameterizedAssignmentCtx) {
+      return parameterizedAssignmentCtx.accept(new ParameterizedAssignmentVisitor());
+    }
   }
 }
