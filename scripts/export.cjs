3a1352fec3373f8f85f0c441077e5c6987efd23e
Seokseong Jeon <sjeon87+github@gmail.com>
2022-06-14 00:17:24 +0900

    export all classes in parser script

--- scripts/export.cjs
+++ scripts/export.cjs
@@ -4,6 +4,6 @@ const { join } = require("path");
 const path = join(__dirname, "../src/parser/asn1Parser.js");
 
 const source = readFileSync(path, 'utf8');
-const updated = source.replace(/^class ModuleDefinitionsContext/gm, 'export class ModuleDefinitionsContext');
+const updated = source.replace(/^class/gm, 'export class');
 
 writeFileSync(path, updated, 'utf8');
