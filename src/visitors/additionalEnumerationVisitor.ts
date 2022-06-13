2b4d4fff50ed4a44e7aa6974b4ad8ea4b74be07b
Seokseong Jeon <sjeon87+github@gmail.com>
2022-06-14 00:47:59 +0900

    add AdditionalEnumerationVIsitor

--- src/visitors/additionalEnumerationVisitor.ts
+++ src/visitors/additionalEnumerationVisitor.ts
@@ -1,11 +1,15 @@
 import { AdditionalEnumerationContext } from "../parser/asn1Parser";
+import { EnumerationVisitor } from "./enumerationVisitor";
 
 /**
  * Grammar
  * ```
+ * enumeration
  * ```
  */
 export class AdditionalEnumerationVisitor {
   public visitChildren(ctx: AdditionalEnumerationContext) {
+    const enumerationCtx = ctx.enumeration();
+    return enumerationCtx.accept(new EnumerationVisitor());
   }
 }
