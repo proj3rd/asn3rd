05044a9bd6eb1b3b37d99b1a56b2b681ef1ba4a8
Seokseong Jeon <sjeon87+github@gmail.com>
2022-06-14 01:13:59 +0900

    add AtNotationVisitor

--- src/visitors/atNotationVisitor.ts
+++ src/visitors/atNotationVisitor.ts
@@ -1,11 +1,18 @@
 import { AtNotationContext } from "../parser/asn1Parser";
+import { ComponentIdListVisitor } from "./componentIdListVisitor";
 
 /**
  * Grammar
  * ```
+ * AT componentIdList
  * ```
  */
 export class AtNotationVisitor {
   public visitChildren(ctx: AtNotationContext) {
+    const componentIdListCtx = ctx.componentIdList();
+    const componentIdList = componentIdListCtx.accept(
+      new ComponentIdListVisitor()
+    );
+    return new AtNotationContext(0, componentIdList);
   }
 }
