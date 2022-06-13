80cb4d470e822de817d4dbc18d68c8e19f987b82
Seokseong Jeon <sjeon87+github@gmail.com>
2022-06-14 01:05:34 +0900

    add AssignmentVisitor

--- src/visitors/assignmentListVisitor.ts
+++ src/visitors/assignmentListVisitor.ts
@@ -1,11 +1,17 @@
-import { AssignmentListContext } from "../parser/asn1Parser";
+import { AssignmentContext, AssignmentListContext } from "../parser/asn1Parser";
+import { AssignmentVisitor } from "./assignmentVisitor";
 
 /**
  * Grammar
  * ```
+ * assignment+
  * ```
  */
 export class AssignmentListVisitor {
   public visitChildren(ctx: AssignmentListContext) {
+    const assignmentCtxList = ctx.assignment();
+    return assignmentCtxList.map((assignmentCtx: AssignmentContext) =>
+      assignmentCtx.accept(new AssignmentVisitor())
+    );
   }
 }
