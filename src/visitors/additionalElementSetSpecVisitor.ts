d3c3d200234ab4c15d3aa87121dde554903731dc
Seokseong Jeon <sjeon87+github@gmail.com>
2022-06-14 00:45:03 +0900

    add AdditionalElementSetSpecVisitor

--- src/visitors/additionalElementSetSpecVisitor.ts
+++ src/visitors/additionalElementSetSpecVisitor.ts
@@ -1,11 +1,15 @@
 import { AdditionalElementSetSpecContext } from "../parser/asn1Parser";
+import { ElementSetSpecVisitor } from "./elementSetSpecVisitor";
 
 /**
  * Grammar
  * ```
+ * elementSetSpec
  * ```
  */
 export class AdditionalElementSetSpecVisitor {
   public visitChildren(ctx: AdditionalElementSetSpecContext) {
+    const elementSetSpecCtx = ctx.elementSetSpec();
+    return elementSetSpecCtx.accept(new ElementSetSpecVisitor());
   }
 }
